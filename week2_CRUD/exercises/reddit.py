153ad545d5067bd20dd703774de6af315e3647cc
ChrisChrisi <chris1cros@gmail.com>
2015-06-09 00:42:02 +0300

    scraping data from reddit

---
+++ week2_CRUD/exercises/reddit.py
@@ -0,0 +1,19 @@
+import pymongo
+import urllib2
+import json
+
+connection = pymongo.MongoClient("mongodb://localhost")
+
+db = connection.reddit
+
+stories = db.stories
+
+stories.drop()
+
+reddit_page = urllib2.urlopen("http://www.reddit.com/r/technology/.json")
+
+parsed = json.loads(reddit_page.read())
+
+for item in parsed['data']['children']:
+    stories.insert_one(item['data'])
+
